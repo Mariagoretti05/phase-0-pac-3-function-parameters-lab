9378fdd21ce2ea17db0380395fb706fe503eb6bb
Mariagoretti Kimani <mgmkimani@gmail.com>
2022-11-17 11:47:48 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(name) {
+    const personName = `Hi, my name is ${name}.`;
+    return personName
+}
+function introductionWithLanguage(name,language) {
+    const studentOneIntroduction= `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    return studentOneIntroduction
+
+}
+function introductionWithLanguageOptional(name,language="JavaScript") {
+    const studentTwoIntroduction = `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    return studentTwoIntroduction
+
+}
+console.log();
